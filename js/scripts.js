65979b4c2446c13cf112f81f9fdf197cfc6ea81e
Kelvin020116 <kelvinchai0116@gmail.com>
2024-03-12 01:39:51 +0800

    Lenis Smooth Scroll

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,16 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
