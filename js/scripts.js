5c3641c79d5be1b5be0b21eef3bb0c7b09afccb4
Kelvin020116 <kelvinchai0116@gmail.com>
2024-03-12 15:07:56 +0800

    lenis

--- js/scripts.js
+++ js/scripts.js
@@ -32,16 +32,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
